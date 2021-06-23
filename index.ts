40fda5784cffce52ef6f28f511ecf56d79d41ca0
Anwesh43 <anweshthecool0@gmail.com>
2021-06-23 15:40:19 +0530

    adding DrawingUtil class

--- index.ts
+++ index.ts
@@ -1,8 +1,16 @@
-const w : number = window.innerWidth 
-const h : number = window.innerHeight 
+const w : number = Math.min(window.innerWidth, window.innerHeight) 
+const h : number = Math.min(window.innerWidth, window.innerHeight)
 const color : string = "#3F51B5"
 const sizeFactor : number = 5.6 
 const delay : number = 20 
 const backColor : string = "#BDBDBD"
+const blocks : number = 30 
+class DrawingUtil {
 
+    static drawNode(context : CanvasRenderingContext2D, scale : number, x : number, y : number) {
+        const size : number = Math.min(w, h) / blocks 
+        context.strokeRect(x, y, size, size)
+        context.fillRect(x, y, size, size * scale)
+    }
 
+}
