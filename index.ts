d79429b752be1ec1a2c0c5f0862d3275a7fa833a
Anwesh43 <anweshthecool0@gmail.com>
2021-06-23 16:24:17 +0530

    adding State class

--- index.ts
+++ index.ts
@@ -5,6 +5,8 @@ const sizeFactor : number = 5.6
 const delay : number = 20 
 const backColor : string = "#BDBDBD"
 const blocks : number = 30 
+const scGap : number = 0.02 
+
 class DrawingUtil {
 
     static drawNode(context : CanvasRenderingContext2D, scale : number, x : number, y : number) {
@@ -43,5 +45,29 @@ class Stage {
         stage.initCanvas()
         stage.render()
         stage.handleTap()
+    }
+}
+
+class State {
+
+    scale : number = 0 
+    dir : number = 0 
+    prevScale : number = 0 
+
+    update(cb : Function) {
+        this.scale += scGap * this.dir 
+        if (Math.abs(this.scale - this.prevScale) > 1) {
+            this.scale = this.prevScale + this.dir 
+            this.dir = 0 
+            this.prevScale = this.scale 
+            cb()
+        }
+    }
+
+    startUpdating(cb : Function) {
+        if (this.dir == 0) {
+            this.dir = 1 - 2 * this.prevScale 
+            cb()
+        }
     }
 }
