8d24218ef6793ebc2f491528900ba60cea95efbd
Anwesh43 <anweshthecool0@gmail.com>
2021-06-23 14:47:12 +0530

    adding constants

---
+++ index.ts
@@ -0,0 +1,8 @@
+const w : number = window.innerWidth 
+const h : number = window.innerHeight 
+const color : string = "#3F51B5"
+const sizeFactor : number = 5.6 
+const delay : number = 20 
+const backColor : string = "#BDBDBD"
+
+
